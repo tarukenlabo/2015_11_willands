df7767a107e05092f203e210b43a1cf5feb2ceb7
hlamworks <a@a.com>
2016-03-07 17:56:52 +0900

    SNS投稿編集中まだ未完成

---
+++ root/ImageObj.php
@@ -0,0 +1,10 @@
+﻿<?php
+	class ImageObj {
+		const UPLOAD_PATH = __DIR__.'/photo/';
+		
+		//ファイル名生成
+		function 
+		
+		//ファイル移動
+		
+	}
--- root/checkin_upload.php
+++ root/checkin_upload.php
@@ -1,7 +1,28 @@
 ﻿<?php
-	var_dump($_POST);
-
 	require "db_connect.php";
+	require "ErrorHandling.php";
+	
+	var_dump($_POST);
+	var_dump($_FILES );
+	
+	$error = new ErrorHandling;
+	$e_message;
+	
+	//ファイルサイズ判定
+	if( $error -> fileSizeDecision( (int)$_FILES["uPhoto"]["size"] ) ){
+		$e_message[] = "アップロードできるファイルサイズの上限20Mを超えています。";
+	}
+	
+	//ファイル拡張子判定
+	if( $error -> fileExtendDecision( (int)$_FILES["uPhoto"]["name"] ) ){
+		$e_message[] = "アップロードできるファイルの種類は画像のみです。";
+	}
+	
+	//一つでもエラーがあればリダイレクト
+	if( is_array( $e_message ) ){
+		header( "Location: ./chek-in_map.php" );
+	}
+
 
 	//データベース接続確立
 	$db = new cls_db();
@@ -9,5 +30,5 @@
 	
 	$dbh -> query( 'SET NAMES utf8' );
 	
-	$insert_sql = INSERT INTO post_check_in(P_ID,U_ID,C_POSIX,C_POSIY,C_DATE,C_COMMENT,M_ID)
+	$insert_sql = 'INSERT INTO post_check_in(P_ID,U_ID,C_POSIX,C_POSIY,C_DATE,C_COMMENT,M_ID) VALUES()';
 
