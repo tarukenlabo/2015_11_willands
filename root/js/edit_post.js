8657dcc03ab3c04f351d9a58e77e4853f519cef4
YONECAN <a@a.com>
2016-03-08 17:56:48 +0900

    ajaxファイル

---
+++ root/js/edit_post.js
@@ -0,0 +1,79 @@
+$(function(){
+
+	//lightboxの設定
+	$(document).on('click','.edit_button',function(event){
+		var target=$(event.target);
+		var edit_id = target.attr('id');
+		$('#wrap').after('<div class="modal_wrap"><div class="modal_box"><p><span id="close">&times;</span></p><div class="modal_content"></div></div></div>');
+
+		$.ajax({
+			type:'POST',
+			url:'./post_detail_show.php',
+			data:{
+				id:edit_id
+			},
+			success:function(data){
+				$('.modal_content').html(data);
+			}
+		});
+		
+		$('#close').click(function(){
+			$('.modal_wrap').fadeOut(500,function(){
+				$(this).remove();
+			});
+		});
+		
+		
+		
+		//削除設定
+		$(document).on('click','.del',function(event){
+			var target=$(event.target);
+			var del_id=target.attr('id');
+			
+			$.ajax({
+				type:'POST',
+				url:'./post_delete.php',
+				data:{
+					id:del_id
+				},
+				success:function(){
+					$('.modal_wrap').fadeOut(500,function(){
+						$(this).remove();
+						$('#checkins #'+del_id).fadeOut();
+					});
+					
+				}
+			});
+		});
+		
+		
+		
+		//更新設定
+		$(document).on('click','.edit',function(event){
+			var target=$(event.target);
+			var edit_id=target.attr('id');
+			
+			$.ajax({
+				type:'POST',
+				url:'./checkin_edit.php',
+				data:{
+					id:edit_id,
+					title:$('#title').val(),
+					comment:$('#c_comment').val()
+				},
+				success:function(){
+					$('.modal_wrap').fadeOut(500,function(){
+						$(this).remove();
+						location.reload();
+					});
+				}
+			
+			});
+		
+		});
+		
+	});
+	
+	
+
+});
