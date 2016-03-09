dfb50e56a3a659734cea169bf7b81da5f95bbad9
tarukenlabo <tarukenlabo@gmail.com>
2016-03-09 14:06:59 +0900

    追加：検索画面（これから作成）

---
+++ root/result.php
@@ -0,0 +1,43 @@
+<?php
+	session_start();
+
+	require_once("./post.php");
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>result.php</title>
+</head>
+<body>
+
+	<div>
+		<h1>たるナビ</h1>
+
+	</div>
+
+	<div>
+		<div>
+<!--
+検索条件
+-->
+		</div>
+		
+		<div>
+<!--
+写真一覧
+-->
+		</div>
+
+		<div>
+<!--
+地図指定
+-->
+		</div>
+	
+	</div>
+
+</body>
+</html>
