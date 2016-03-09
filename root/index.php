79b74d9d5b7085bcdd3994a1fc574412eb5bda49
yukari-sato-willans <a@a.com>
2016-03-09 11:22:57 +0900

    分割前のファイルです

---
+++ root/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="./style.css" type="text/css">
+		<title>(★各ページタイトル入れてください)</title>
+	<head>
+	<body>
+		<div id="wrap">
+			<header>
+				<p>(★ヘッダーです)</p>
+			</header>
+
+			<nav>
+				<ul class="clearFix">
+					<li>(★地図で選ぶ)</li>
+					<li>(★カテゴリーで選ぶ)</li>
+					<li>(★キーワードで選ぶ)</li>
+				</ul>
+			</nav>
+			
+			<article>
+				<p>(★メインコンテンツです)</p>
+			</article>
+			
+			<footer>
+				<p>(★フッターです)</p>
+			</footer>
+		</div><!--#wrap-->
+	</body>
+</html>
