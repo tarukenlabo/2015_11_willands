3b85080669422382eeef31e7f2e74c69b44cf81b
shinji1006 <a@a.com>
2016-03-09 17:56:38 +0900

    ファイル更新

---
+++ doc/22_デザイン設計/httm&css/header.php
@@ -0,0 +1,18 @@
+<header>
+				<div class="header_bar">
+				</div>
+
+				
+				<div class="search">
+					
+					<form action="/" name="search" method="get" class="search_form">
+					<dl class="search">
+					<dt><input type="text" name="search" value="" placeholder="検索" /></dt>
+					<dd><button><span></span></button></dd>
+					</dl>
+					</form>
+					
+					<img src="./img/sea.jpg">
+					<img class="logo" src="./img/tarunavi_logo.png">
+				</div>
+			</header>
