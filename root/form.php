e4dd4592a991e231c31b7708c41ed0071a8fc925
kaz-willands <oikazuya@hotmail.com>
2016-03-09 09:53:07 +0900

    会員登録は Email とパスワードのみに変更

--- root/form.php
+++ root/form.php
@@ -10,11 +10,11 @@
 <!--    <tr>
 	      <td class="item">ユーザー名：</td>
 	      <td><input type="text" size="30" name="input_" value="<?php if (isset($input_userid)):print $input_userid; endif; ?>"></td>
-	    </tr> -->
+	    </tr> 
 	    <tr>
 	      <td class="item">アカウント：</td>
 	      <td><input type="text" size="30" name="input_name" value="<?php if (isset($input_name)):print $input_name; endif; ?>"></td>
-	    </tr>
+	    </tr> -->
 	    <tr>
 	      <td class="item">E-mail：</td>
 	      <td><?php if (isset($email)): print $email; else: ?><input type="text" size="30" name="input_mail"><?php endif; ?></td>
