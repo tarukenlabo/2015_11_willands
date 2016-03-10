48f22262b1ce11e1f6582604e4955a2a7d221fd2
tarukenlabo <tarukenlabo@gmail.com>
2016-03-10 14:42:54 +0900

    作成中：プロフィール画面。残件＝1、アイコン登録、2、入力チェック、3戻るボタン

--- root/profile_mod.php
+++ root/profile_mod.php
@@ -4,6 +4,8 @@
 	//セッション開始
 	session_start();
 	
+	$_SESSION["user_data"] = $_POST;
+
 	$name = $_POST["name"];
 	$age = $_POST["age"];
 	$sex = $_POST["sex"];
@@ -11,7 +13,7 @@
 	$icon = ( $_FILES["icon"] );
 	var_dump($_POST);
 	echo "<br>";
-	var_dump(icon);
+	var_dump($icon);
 
 ?>
 
@@ -23,8 +25,8 @@
 	</head>
 	
 	<body>
-	<form action="./profile_regist.php" method="post" enctype="multipart/form-data">
-		<p>名前　<input type="text" name="name" value="<?php echo $name; ?>"></p>
+	<form action="./profile_comp.php" method="post" enctype="multipart/form-data">
+		<p>名前　<?php echo $name; ?></p>
 		<p>年齢　<?php echo $age; ?></p>
 		<p>性別　<?php 
 					if($sex == 1){
@@ -34,7 +36,6 @@
 					} ?></p>
 		<p>自己紹介 <?php echo $self; ?></p>
 		<p>アイコン<?php echo $icon; ?></p>
-		<p><input type="button" value="編集に戻る" onclick="./profile.php"> 
 		 <input type="submit" value="これで登録"> 
 	</form>
 
