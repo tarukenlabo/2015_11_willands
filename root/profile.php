4c7b6050f9b672b1393cf01162431d73431fa921
tarukenlabo <tarukenlabo@gmail.com>
2016-03-10 13:01:01 +0900

    プロフィール画面、開発中

--- root/profile.php
+++ root/profile.php
@@ -2,7 +2,16 @@
 	//セッション開始
 	session_start();
 	
- require_once("./db_connect.php");
+	require_once("./user.php");
+
+	$u_info = search_user_by_uid($_SESSION['u_id']);
+
+	$u_name = $u_info['U_NAME'];
+	$u_age = $u_info['U_AGE'];
+	$u_sex = $u_info['U_SEX'];
+	$u_comment = $u_info['U_COMMENT'];
+	$u_thumb = $u_info['U_THUMB'];
+
 ?>
 
 <DOCTYPE html>
@@ -13,11 +22,14 @@
 	</head>
 	
 	<body>
-	<form action="./profile_check.php" method="post" enctype="multipart/form-data">
-		<p>名前<input name="name" type="text" value=""></p>
-		<p>年齢<input name="age" type="number" value=""></p>
-		<p>性別　男性<input name="sex" type="radio" value="1">女性<input name="sex" type="radio" value="2"></p>
-		<p>自己紹介</p><textarea name="self"></textarea>
+	<form action="./profile_mod.php" method="post" enctype="multipart/form-data">
+		<p>名前<input name="name" type="text" value=<?php echo $u_name;?>></p>
+		<p>年齢<input name="age" type="number" value=<?php echo $u_age;?>></p>
+		<p>性別
+				男性<input name="sex" type="radio" value="1" <?php if($u_sex == 1){ print "checked";}?>>
+				女性<input name="sex" type="radio" value="2" <?php if($u_sex == 2){ print "checked";}?>>
+		</p>
+		<p>自己紹介</p><textarea name="self"><?php echo $u_comment;?></textarea>
 		<p>アイコン<input name="icon" type="file"></p>
 		<input type="submit" value="確認画面へ">
 	</form>
