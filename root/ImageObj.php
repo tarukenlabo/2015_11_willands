c10e29b86c01bd327c2c5832c963f74e946b3ac1
tarukenlabo <tarukenlabo@gmail.com>
2016-03-10 16:17:16 +0900

    作成中：プロフィール更新。残件＝画像登録、チェック処理、戻るボタン

--- root/ImageObj.php
+++ root/ImageObj.php
@@ -12,4 +12,21 @@
 			move_uploaded_file( $upload_image, './photo/'.$image_name );
 			return './photo/'.$image_name;
 		}
+
+
+		//ファイル名生成（サムネイル画像）
+		public function imageRenameForThumb( $image_name,$user_id){
+			$rename = time()."_". $user_id . "_".$image_name;
+			
+			return $rename;
+		}
+		
+		//ファイル移動（サムネイル画像）
+		public function imageMoveForThumb( $upload_image,$image_name ){
+			move_uploaded_file( $upload_image, './thumb/'.$image_name );
+			return './thumb/'.$image_name;
+		}
+
+	}
+	
 	}
--- root/profile_comp.php
+++ root/profile_comp.php
@@ -11,7 +11,35 @@
 	$age = $user_data["age"];
 	$sex = $user_data["sex"];
 	$self = $user_data["self"];
-//	$icon = $user_data["icon"];
+//	$thumb = $user_data["thumb"];
+
+/*
+	//アイコン取込処理
+
+	echo "<p>";
+
+	var_dump($_FILES["thumb"]);
+
+	echo "</p>";
+	
+	//ファイルサイズ判定
+	if( $error -> fileSizeDecision( (int)$_FILES["thumb"]["size"] ) ){
+		$e_message[] = "アップロードできるファイルサイズの上限20Mを超えています。";
+	}
+	
+	//ファイル拡張子判定
+	if( $error -> fileExtendDecision( (int)$_FILES["thumb"]["name"] ) ){
+		$e_message[] = "アップロードできるファイルの種類は画像のみです。";
+	}
+	
+	//一つでもエラーがあればリダイレクト
+	if( is_array( $e_message ) ){
+		header( "Location: ./chek-in_map.php" );
+	}
+	
+	//アップロード画像リネーム
+	$image_name = $image_obj -> imageRenameForThumb( $_FILES["thumb"]["name"],$u_id );
+*/
 	
 	//データベース接続設定
 	require_once("./db_connect.php");
@@ -19,12 +47,26 @@
 	$dbh = $db->db_connect();
 	$dbh->query('SET NAMES utf8');
 
+    //トランザクション処理を開始
+    $dbh->beginTransaction();
 
+	//u_auth更新
+	$sql= "UPDATE u_auth SET "
+				. "U_NAME = \"" . $name . "\" "
+			. "WHERE "
+			. "U_ID = " . $u_id;
+
+	echo $sql;
+
+	$stmt = $dbh->query($sql);
+	$stmt->execute();
+
+	//u_info更新
 	$sql= "UPDATE u_info SET "
-				. "U_AGE = " . $age . ", "
-				. "U_SEX = " . $sex . ", "
-				. "U_COMMENT = \"" . $self . "\" "
-//			. "U_THUMB = \"" . $icon . "\", "
+			. "U_AGE = " . $age . ", "
+			. "U_SEX = " . $sex . ", "
+			. "U_COMMENT = \"" . $self . "\", "
+			. "U_THUMB = \"" . $image_name . "\" "
 			. "WHERE "
 			. "U_ID = " . $u_id;
 
@@ -32,7 +74,18 @@
 	
 	$stmt = $dbh->query($sql);
 	$stmt->execute();
+
+	//コミット
+	$dbh->commit();
+
+
+/*
+	//TMPファイル移動
+	$upload_file_path = $image_obj -> imageMoveForThumb( $_FILES["thumb"]["tmp_name"], $image_name );
 	
+	$_SESSION['$thumb'] = $upload_file_path;
+*/
+
 ?>
 
 <DOCTYPE html>
