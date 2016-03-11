0be725030decd10055d6b5efbb933c873a2cec6e
yukari-sato-willans <yuka_rin0814@hotmail.com>
2016-03-11 14:58:21 +0900

    更新しました

--- root/comment-form.php
+++ root/comment-form.php
@@ -4,19 +4,41 @@
 	$p_id = $_GET['p_id'];
 ?>
 
-<DOCTYPE html>
+<!DOCTYPE html>
 <html lang="ja">
 	<head>
-		<title>コメントフォーム</title>
-	</head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="./css/sato-style_oocss.css" type="text/css">
+		<link rel="stylesheet" href="./css/style.css" type="text/css">
+		<title>コメントページ</title>
+	<head>
 	<body>
-	<p>テキスト</p>
-	
-	<p>以上の内容でよろしければ</p>
-	<p>送信ボタンを押してください</p>
-	<form action="./comment-form-mod.php?p_id=<?php echo $p_id; ?>" method="post" enctype="multipart/form-data">
-		<textarea name="comment"></textarea>
-		<input type="submit" value="送信">
-	</form>
-	</body>
+		<div id="wrap" class="align-center orange">
+			<?php require 'header.php' ; ?>
+
+			<nav>
+				<ul>
+					<li class="l-float clear-text">(★地図で選ぶ)</li>
+					<li class="l-float clear-text">(★カテゴリーで選ぶ)</li>
+					<li class="l-float clear-text">(★キーワードで選ぶ)</li>
+				</ul>
+			</nav>
+
+			<article class="clearFix white">
+				<h2 class="contents-title align-c">コメント</h2>
+
+				<div class="box shadow">
+				
+					<div class="align-c">
+						<form action="./comment-form-mod.php?p_id=<?php echo $p_id; ?>" method="post" enctype="multipart/form-data">
+							<textarea name="comment" class="box-line4"></textarea>
+							<p class="other-text align-c">以上の内容でよろしければ送信ボタンを押してください</p>
+							<div class="align-c"><input type="submit" value="送 信" class="button_120 white align-center"></div>
+						</form>
+				</div> <!--.box shadow-->
+			</artcle>
+			
+			<?php require 'footer.php' ; ?>
+		</div><!--#wrap-->
+	</body>	
 </html>
