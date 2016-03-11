7876e998e4c4a2580ba7cdf4320605ff32359ad3
YONECAN <a@a.com>
2016-03-11 09:59:58 +0900

    ブックマーク解除プログラム

---
+++ root/bkm_delete.php
@@ -0,0 +1,20 @@
+<?php
+	require_once("./db_connect.php");
+	require_once("./functions.php");
+	
+	$db = new cls_db();
+	$dbh = $db->db_connect();
+	$dbh->query('SET NAMES utf8');
+
+	session_start();
+	
+	$p_id = h($_GET['p_id']);
+	$u_id = 18;
+	
+	$delbkm = "DELETE FROM bookmark WHERE P_ID = ".$p_id."&& U_ID = ".$u_id;
+	$stmt = $dbh->prepare($delbkm);
+	$stmt->execute();
+	echo $delbkm;
+	$url = $_SERVER['HTTP_REFERER'];
+	
+	header("Location:$url");
