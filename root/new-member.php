9be0cf7a8f36a1c8b265c7690e18486e5b675d6f
yukari-sato-willans <a@a.com>
2016-03-11 09:55:08 +0900

    まだ編集中です

--- root/new-member.php
+++ root/new-member.php
@@ -1,25 +1,54 @@
 <!DOCTYPE html>
-<html>
-<head></head>
-<body>
-	<form method="post" action="regist.php">
-	  <input type="hidden" name="mode" value="regist_confirm">
-	  <table>
-	    <caption>会員情報登録フォーム</caption>
-	    <tr>
-	      <td class="item">E-mail：</td>
-	      <td><?php if (isset($email)): print $email; else: ?><input type="text" size="30" name="input_mail"><?php endif; ?></td>
-	    </tr>
-	    <tr>
-	      <td class="item">パスワード：</td>
-	      <td><input type="password" size="30" name="input_pass" value="<?php if (isset($input_pass)):print $input_pass; endif; ?>">&nbsp;&nbsp;※ 6文字以上16文字以下</td>
-	    </tr>
-	    <tr>
-	      <td class="item">パスワード（確認）：</td>
-	      <td><input type="password" size="30" name="pass_conf" value="<?php if (isset($pass_conf)):print $pass_conf; endif; ?>">&nbsp;&nbsp;※ 6文字以上16文字以下</td>
-	    </tr>
-	  </table>
-	  <div><input type="submit" name="submit" value=" 送 信 "></div>
-	</form>
-</body>
+<html lang="ja">
+
+	<head>
+		<meta charset="UTF=8">
+		<link rel="stylesheet" href="./css/style_oocss.css" type="text/css">
+		<title>会員登録</title>
+	</head>
+	
+	<body>
+		<div id="wrap" class="arlign_center">
+			<header>
+				<p>(★ヘッダーです)</p>
+			</header>
+			
+			<nav>
+				<ul>
+					<li class="l-float clear_text">(★地図で選ぶ)</li>
+					<li class="l-float clear_text">(★カテゴリーで選ぶ)</li>
+					<li class="l-float clear_text">(★キーワードで選ぶ)</li>
+				</ul>
+			</nav>
+
+			<article class="clearFix align_center">
+				<div class="box shadow">
+				<form method="post" action="regist.php">
+					<input type="hidden" name="mode" value="regist_confirm">
+						<table>
+							<caption class="contents-title">会員情報登録フォーム</caption>
+						<tr>
+							<td class="article-title">メールアドレス</td>
+							<td class="text-box-1line"><?php if (isset($email)): print $email; else: ?><input type="text" size="30" name="input_mail"><?php endif; ?></td>
+						</tr>
+						<tr>
+							<td class="article-title">パスワード</td>
+							<td class="text-box-1line"><input type="password" size="30" name="input_pass" value="<?php if (isset($input_pass)):print $input_pass; endif; ?>">&nbsp;&nbsp;※ 6文字以上16文字以下</td>
+						</tr>
+						<tr>
+							<td class="article-title">パスワード（確認）</td>
+							<td class="text-box-1line"><input type="password" size="30" name="pass_conf" value="<?php if (isset($pass_conf)):print $pass_conf; endif; ?>">&nbsp;&nbsp;※ 6文字以上16文字以下</td>
+						</tr>
+						</table>
+						<div><input type="submit" name="submit" value=" ログイン "></div>
+				</form>
+				</div>
+			</article>
+
+		
+		<footer>
+			<p>(★フッターです)</p>
+		</footer>
+		</div><!--#wrap-->
+	</body>
 </html>
