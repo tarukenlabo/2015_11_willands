8bb4974e3add23e8fc14f7024a8ee7dc2acc28ad
shinji1006 <a@a.com>
2016-03-14 13:59:25 +0900

    footer更新

--- root/footer.php
+++ root/footer.php
@@ -1,18 +1,25 @@
-			<footer class="l-float">
-				<div class="fotter_logo l-float">
-					<img src="./img/tarunavi_logo_footer.png" >
+			<footer class="clearFix">
+				<div class="fotter_logo l-float white">
+					<img src="./img/tarunavi_logo.png" >
+					
+					<div class="r-float">
+						<ul class="">
+							<li class="l-float margin10"><a class="foot-text" href="./index.php">たるナビトップ</a></li>
+							<li class="l-float margin10"><a class="foot-text" href="./log-in.php">ログイン</a></li>
+							<li class="l-float margin10"><a class="foot-text" href="./new-member.php">会員登録</a></li>
+							<li class="l-float margin10"><a class="foot-text" href="./term.php">利用規約</a></li>
+							<li class="l-float margin10"><a class="foot-text" href="./policy.php">プライバシーポリシー</a></li>
+							<li class="l-float margin10"><a class="foot-text" href="./faq.php">ご利用ガイド</a></li>
+							<li class="l-float margin10"><a class="foot-text" href="./contact.php">お問い合わせ</a></li>
+						</ul>
+						<div class="foot-anime foot-anime2">
+							<img src="./img/l_019.png" width="100">
+						</div>
+					</div>
+					
 				</div>
 				
-					<ul class="l-float footer_nav">
-						<li><a href="#">たるナビトップ</a></li>
-						<li><a href="#">ログイン</a></li>
-						<li><a href="#">会員登録</a></li>
-						<li><a href="#">利用規約</a></li>
-						<li><a href="#">プライバシーポリシー</a></li>
-						<li><a href="#">FAQ</a></li>
-						<li><a href="#">お問い合わせ</a></li>
-					</ul>
-				
+					
 			</footer>
 		</div>
 	</body>	
