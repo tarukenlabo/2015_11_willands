fd60acd494917cb49a4af0680cd40dc4a8275c43
YONECAN <a@a.com>
2016-03-14 12:19:57 +0900

    ログアウト機能追加

--- root/footer.php
+++ root/footer.php
@@ -5,7 +5,11 @@
 				
 					<ul class="l-float footer_nav">
 						<li><a href="./index.php">たるナビトップ</a></li>
-						<li><a href="./log-in.php">ログイン</a></li>
+						<?php if(isset($_SESSION['u_id'])): ?>
+							<li><a href="./log-out.php">ログアウト</a></li>
+						<?php else: ?>
+							<li><a href="./log-in.php">ログイン</a></li>
+						<?php endif; ?>
 						<li><a href="./new-member.php">会員登録</a></li>
 						<li><a href="./terms.php">利用規約</a></li>
 						<li><a href="./policy.php">プライバシーポリシー</a></li>
